b303c3e3048aa93dbcfd57fc426b04fbf04c564e
Buretta <brett@magnumcreative.ca>
2014-04-15 14:44:38 -0700

    adjusted container to container-fluid

--- wrk/themes/magnum/base.php
+++ wrk/themes/magnum/base.php
@@ -17,7 +17,7 @@
     }
   ?>
 
-  <div class="wrap container" role="document">
+  <div class="wrap container-fluid" role="document">
     <div class="content row">
       <?php get_template_part('templates/snippet-breadcrumbs'); ?>
       <main class="main <?php echo roots_main_class(); ?>" role="main">
